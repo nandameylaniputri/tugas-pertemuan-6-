8dd014ee739c94810b7d5bb24efeb92c1419a822
nandameylaniputri <meylaniputrinanda@gmail.com>
2024-06-04 10:03:09 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+//1.Clg (console.log)
+console.log("Hello World");
+
+//var,let,const
+let buttonSubmit = 2+4;
+console.log(buttonSubmit);
+buttonSubmit="7";
+console.log(buttonSubmit);
+
+const data = 100-90
+console.log(data);
+
+
+
+function buttonSubmit(){
+    alert("Data sudah dikirim");
+    alert("Data sudah dikirim");
+}
